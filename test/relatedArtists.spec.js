8235cc1babf1c7b2a59ce1ddd215c86ec28ef6c7
kgabor95 <koszorus.gabor95@gmail.com>
2020-11-29 19:56:21 +0100

    Test data added

--- test/relatedArtists.spec.js
+++ test/relatedArtists.spec.js
@@ -6,6 +6,7 @@ const { schemaValidation, randomItem } = require("../todo/spotify/helper/helpers
 const relatedArtistsSchema = require("../data/schema/relatedArtists.json");
 const testData = require("../data/testData/relatedArtists.json");
 const constants = require("../data/testData/constants.json");
+const invalidTestData = require("../data/testData/relatedArtistsInvalid.json");
 
 let spotify;
 before(async () => {
@@ -13,23 +14,21 @@ before(async () => {
 });
 
 describe("Spotify API test", () => {
-    describe("Test of Related Artists API", () => {
-        describe("Get an Artist's Related Artists", () => {
-            for (const artist of testData) {
-                it("should return the proper status code", async () => {
-                    // eslint-disable-next-line max-len
-                    const response = await spotify.get(`${constants.getAnArtistUrl}${artist.artistID}${constants.getRelatedArtists}`);
-                    expect(response.status).to.be.equal(StatusCodes.OK);
-                });
-            }
-
-            it("should return a valid response", async () => {
-                const selectedArtist = randomItem(testData);
+    describe("Get an Artist's Related Artists", () => {
+        for (const artist of testData) {
+            it(`should return the proper status code: ${artist.artistID}`, async () => {
                 // eslint-disable-next-line max-len
-                const response = await spotify.get(`${constants.getAnArtistUrl}${selectedArtist.artistID}${constants.getRelatedArtists}`);
-                const responseData = response.data;
-                expect(await schemaValidation(responseData, relatedArtistsSchema)).to.be.true;
+                const response = await spotify.get(`${constants.getAnArtistUrl}${artist.artistID}${constants.getRelatedArtists}`);
+                expect(response.status).to.be.equal(StatusCodes.OK);
             });
+        }
+
+        it("should return a valid response", async () => {
+            const selectedArtist = randomItem(testData);
+            // eslint-disable-next-line max-len
+            const response = await spotify.get(`${constants.getAnArtistUrl}${selectedArtist.artistID}${constants.getRelatedArtists}`);
+            const responseData = response.data;
+            expect(await schemaValidation(responseData, relatedArtistsSchema)).to.be.true;
         });
 
         it("should return the artists related artists name in the expected order", async () => {
@@ -39,6 +38,16 @@ describe("Spotify API test", () => {
             // TODO: Go through the whole array
             expect(response.data.artists[1].name).to.be.equal(selectedArtist.output.name[1]);
         });
+
+        // outline
+        for (const artist of invalidTestData) {
+            it(`should return the proper error code in case of invalid id: " ${artist.artistID}`, async () => {
+                const response = await spotify
+                    .get(`${constants.getAnArtistUrl}${artist.artistID}${constants.getRelatedArtists}`)
+                    .catch(err => err.response);
+                const statusCode = StatusCodes[artist.output.status];
+                expect(response.status).to.be.equal(statusCode);
+            });
+        }
     });
 });
-
--- test/topTracks.spec.js
+++ test/topTracks.spec.js
@@ -4,6 +4,8 @@ const { init } = require("../todo/spotify/helper/auth");
 const { StatusCodes } = require("http-status-codes");
 const testData = require("../data/testData/topTracks.json");
 const constants = require("../data/testData/constants.json");
+const artistsSchema = require("../data/schema/topTracks.json");
+const { schemaValidation, randomItem } = require("../todo/spotify/helper/helpers");
 // eslint-disable-next-line no-loop-func
 let spotify;
 before(async () => {
@@ -26,4 +28,21 @@ describe("Get an Artist's Top Tracks", () => {
             expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
         });
     }
+
+    it("should return a valid response", async () => {
+        const selectedArtist = randomItem(testData);
+        // eslint-disable-next-line max-len
+        const response = await spotify.get(`${constants.getAnArtistUrl}${selectedArtist.artistID}${constants.getTopTracks}${constants.getTopTracksMarket}${selectedArtist.market}`);
+        const responseData = response.data;
+        expect(await schemaValidation(responseData, artistsSchema)).to.be.true;
+    });
+
+    it(`should return the Artist's top track`, async () => {
+        const selectedArtist = randomItem(testData);
+        // eslint-disable-next-line max-len
+        const response = await spotify.get(`${constants.getAnArtistUrl}${selectedArtist.artistID}${constants.getTopTracks}${constants.getTopTracksMarket}${selectedArtist.market}`);
+        expect(response.data.tracks[0].album.name).to.be.equal(selectedArtist.output.album.name[0]);
+        expect(response.status).to.be.equal(StatusCodes.OK);
+    });
 });
+
